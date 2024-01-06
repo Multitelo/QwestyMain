2e10376fd47b9edc6bedeaabb2dd021f45fc4d20
chinwaneti <chidinmanwaneti,cn@gmail.com>
2024-01-06 20:59:27 +0100

    feat: add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './components/Footer';
 import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
         <HomePage />
+        <Footer />
     </div>
   );
 }
