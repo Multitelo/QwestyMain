275a135b284a112f027f8c1b8ad70644537cd2a9
[mahtot] <[mahtotgher13@gmail.com]>
2024-03-18 18:34:40 +0300

    Completed the preview survey page

--- Qwesty/src/components/mahtot/PreviewSurvey.jsx
+++ Qwesty/src/components/mahtot/PreviewSurvey.jsx
@@ -1,27 +1,30 @@
 import Question from "./Question"
 import { useTheme } from "../../context/ThemeContext"
+import { IoArrowBack } from "react-icons/io5";
 
 function PreviewSurvey() {
     const {resTheme} = useTheme();
     const questionTypes = [
                         {title:'Q1', type:'Multiple choice'},
-                        {title:'Q2', type:"Checkboxed"},
+                        {title:'Q2', type:"Checkboxes"},
                         {title:'Q3', type:'Short text'},
                         {title:'Q4', type:"Long text"}]
   return (
-
+    
     <div className={`preview-survey-container ${resTheme}`}>
+        <IoArrowBack size={'2rem'}/>
         <h1>Preview your survey</h1>
         <p> You'll be able to edit this draft before you send it out.</p>
         <h2>Questions</h2>
-        {
+        <div className="question-component">
+         {
             questionTypes.map((item, index)=>(
                 <Question title={item.title}
                           type={item.type}/>
             ))
         }
-
-       <div>
+            </div>
+       <div className="preview">
            <button>set deadline</button> <span> (optional)</span>
         </div> 
 
