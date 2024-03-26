ac9290f3c59bb31709461714ce13156ee82f2912
[mahtot] <[mahtotgher13@gmail.com]>
2024-03-26 22:40:13 +0300

    removed mui

--- Qwesty/src/components/mahtot/CalendarMu.jsx
+++ Qwesty/src/components/mahtot/CalendarMu.jsx
@@ -1,27 +1,27 @@
 
-import * as React from 'react';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
-import dayjs from 'dayjs';
+// import * as React from 'react';
+// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+// import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+// import dayjs from 'dayjs';
 
-export default function CalendarM({deadline, setDeadline, handleClose}) {
- const currentDate = dayjs();
-  return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <StaticDatePicker 
-         orientation="portrait" 
+// export default function CalendarM({deadline, setDeadline, handleClose}) {
+//  const currentDate = dayjs();
+//   return (
+//     <LocalizationProvider dateAdapter={AdapterDayjs}>
+//       <StaticDatePicker 
+//          orientation="portrait" 
         
-        value={deadline}
-                      onChange={(date)=>setDeadline(date)}
-                      onClose={handleClose}
-                      minDate={currentDate}
-                     inputFormat="MM/dd/yyyy"
+//         value={deadline}
+//                       onChange={(date)=>setDeadline(date)}
+//                       onClose={handleClose}
+//                       minDate={currentDate}
+//                      inputFormat="MM/dd/yyyy"
         
         
-      />
-    </LocalizationProvider>
-  );
-}
+//       />
+//     </LocalizationProvider>
+//   );
+// }
 
                       
