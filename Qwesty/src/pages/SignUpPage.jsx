87ed9c2ff846f4ea80adebd0c71c93d70a061887
Multitelo <emenikeaugustine301@gmail.com>
2024-03-29 20:13:31 +0100

    added solvety

--- Qwesty/src/pages/SignUpPage.jsx
+++ Qwesty/src/pages/SignUpPage.jsx
@@ -52,7 +52,7 @@ function SignUpPage() {
         formData.append('usertype', usertype);
         
         try {
-            const response = await fetch('http://localhost/qwestymain/api/email.php', {
+            const response = await fetch('https://solvety.info/api/email.php', {
                 method: 'POST',
                 body: formData,
             });
@@ -85,7 +85,7 @@ function SignUpPage() {
         formData.append('usertype', usertype);
     
         try {
-            const response = await fetch('http://localhost/qwestymain/api/uname.php', {
+            const response = await fetch('https://solvety.info/api/uname.php', {
                 method: 'POST',
                 body: formData,
             });
@@ -146,7 +146,7 @@ function SignUpPage() {
         formData.append('password', password.value);
     
         try {
-            const response = await fetch('http://solvety.info/api/signin.php', {
+            const response = await fetch('https://solvety.info/api/signin.php', {
                 method: 'POST',
                 body: formData,
             });
